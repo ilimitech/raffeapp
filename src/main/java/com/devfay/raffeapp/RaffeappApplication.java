23ec41f10719971df66e88a4649817ffc1bd0d63
Anthony <anthony@ilimitech.com>
2024-07-15 20:57:47 +0200

    base commit

---
+++ src/main/java/com/devfay/raffeapp/RaffeappApplication.java
@@ -0,0 +1,13 @@
+package com.devfay.raffeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaffeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaffeappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devfay/raffeapp/RaffeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfay.raffeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaffeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
